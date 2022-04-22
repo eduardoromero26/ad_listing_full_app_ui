110924396365e53ab2182367d761a3c968c50a75
eduardoromero26 <65006253+eduardoromero26@users.noreply.github.com>
2022-04-22 07:55:55 -0500

    Update ProductCardWidget.dart

--- lib/custom-widgets/ProductCardWidget.dart
+++ lib/custom-widgets/ProductCardWidget.dart
@@ -37,7 +37,7 @@ class ProductCardWidget extends StatelessWidget {
           Container(
               height: double.infinity,
               width: double.infinity,
-              child: Image.asset(
+              child: Image.network(
                 imageURL,
                 fit: BoxFit.cover,
               )),
